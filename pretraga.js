4c0b6603e961becb6595c206264afa9fa689ec51
vesnapopovic <58085815+vesnapopovic@users.noreply.github.com>
2020-03-04 18:34:43 +0100

    Update pretraga.js
    dodate funkcije buttonim-a
    dodata promenljiva filmovi i rad van fetch-a

--- pretraga.js
+++ pretraga.js
@@ -1,88 +1,136 @@
+
+var filmovi = [];
+
 fetch('https://baza-filmova.herokuapp.com/filmovi/')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-
-        // prikaz filmova na celoj stranici u vise kolona
-
-
-        prikazFilmova = document.getElementById('iPrikazFilmova');
-        for (let i = 0; i < data.length; i++) {
-            prikazFilmova.innerHTML += (`${data[i].naziv}<br><br><img src="${data[i].slika}" alt=""><br><br>${data[i].godina}<br><br><br>`);
-
-        }
-
-
-
-        //pretraga filmova
-
-
-        function pronadji() {
-            prikazFilmova.innerHTML = ""
-            var trazeniFilm = document.getElementById('iPronadjiFilm').value;
-
-
-            rezultatPretrage = [];
-
-            for (let i = 0; i < data.length; i++) {
-                var naziv = data[i].naziv.toLowerCase();
-                rezultat = naziv.includes(trazeniFilm);
-
-                if (rezultat == true) {
-                    rezultatPretrage.push(data[i]);
-                }
-            }
-
-
-            for (let j = 0; j < rezultatPretrage.length; j++) {
-                prikazFilmova.innerHTML += (`${rezultatPretrage[j].naziv}<br><br><img src="${rezultatPretrage[j].slika}" alt=""><br><br>${rezultatPretrage[j].godina}<br><br><br> `);
-
-            }
-
-        }
-
-        pretraga = document.getElementById('iPronadjiFilm');
-        pretraga.addEventListener('input', pronadji);
-
-        //sortiranje filmova po godini
-
-        function sortirajUzlazno() {
-            data.sort(function (a, b) { return a.godina - b.godina });
-            prikazFilmova.innerHTML = "";
-            for (let i = 0; i < data.length; i++) {
-                prikazFilmova.innerHTML += (`${data[i].naziv}<br><br><img src="${data[i].slika}" alt=""><br><br>${data[i].godina}<br><br><br>`);
-
-            }
-        }
-
-        function sortirajSilazno() {
-            data.sort(function (a, b) { return b.godina - a.godina });
-            prikazFilmova.innerHTML = "";
-            for (let i = 0; i < data.length; i++) {
-                prikazFilmova.innerHTML += (`${data[i].naziv}<br><br><img src="${data[i].slika}" alt=""><br><br>${data[i].godina}<br><br><br>`);
-
-            }
-        }
-
-        select = document.getElementById('iGodina');
-        select.addEventListener('change', odaberi)
-        console.log(select.selectedIndex);
-        function odaberi() {
-            if (select.selectedIndex == 1) {
-                sortirajUzlazno();
-
-            }
-            if (select.selectedIndex == 2) {
-                sortirajSilazno();
-            }
-
-        }
-
-
-
-
-
-
-
-
+        filmovi = data;
+        console.log(filmovi);
     })
+
+// prikaz filmova na celoj stranici u vise kolona
+
+
+prikazFilmova = document.getElementById('iPrikazFilmova');
+for (let i = 0; i < filmovi.length; i++) {
+    prikazFilmova.innerHTML += (`${filmovi[i].naziv}<br><br><img src="${filmovi[i].slika}" alt=""><br><br>${filmovi[i].godina}<br><br><br>`);
+
+}
+
+
+
+//pretraga filmova
+
+
+function pronadji() {
+    prikazFilmova.innerHTML = ""
+    var trazeniFilm = document.getElementById('iPronadjiFilm').value;
+
+
+    rezultatPretrage = [];
+
+    for (let i = 0; i < filmovi.length; i++) {
+        var naziv = filmovi[i].naziv.toLowerCase();
+        rezultat = naziv.includes(trazeniFilm);
+
+        if (rezultat == true) {
+            rezultatPretrage.push(filmovi[i]);
+        }
+    }
+
+
+    for (let j = 0; j < rezultatPretrage.length; j++) {
+        prikazFilmova.innerHTML += (`${rezultatPretrage[j].naziv}<br><br><img src="${rezultatPretrage[j].slika}" alt=""><br><br>${rezultatPretrage[j].godina}<br><br><br> `);
+
+    }
+
+}
+
+pretraga = document.getElementById('iPronadjiFilm');
+pretraga.addEventListener('input', pronadji);
+
+//sortiranje filmova po godini
+
+function sortirajUzlazno() {
+    filmovi.sort(function (a, b) { return a.godina - b.godina });
+    prikazFilmova.innerHTML = "";
+    for (let i = 0; i < filmovi.length; i++) {
+        prikazFilmova.innerHTML += (`${filmovi[i].naziv}<br><br><img src="${filmovi[i].slika}" alt=""><br><br>${filmovi[i].godina}<br><br><br>`);
+
+    }
+}
+
+function sortirajSilazno() {
+    filmovi.sort(function (a, b) { return b.godina - a.godina });
+    prikazFilmova.innerHTML = "";
+    for (let i = 0; i < filmovi.length; i++) {
+        prikazFilmova.innerHTML += (`${filmovi[i].naziv}<br><br><img src="${filmovi[i].slika}" alt=""><br><br>${filmovi[i].godina}<br><br><br>`);
+
+    }
+}
+
+najstariji = document.getElementById('iNajstariji');
+najmladji = document.getElementById('iNajmladji');
+najstariji.addEventListener('click', sortirajUzlazno);
+najmladji.addEventListener('click', sortirajSilazno);
+
+
+
+//sortiranje filmova po nazivu
+
+
+function sortirajZA() {
+    filmovi.sort(function (a, b) {
+        var nameA = a.naziv.toUpperCase();
+        var nameB = b.naziv.toUpperCase();
+        if (nameA > nameB) {
+            return -1;
+        }
+        if (nameA < nameB) {
+            return 1;
+        }
+
+
+        return 0;
+    });
+    prikazFilmova.innerHTML = "";
+    for (let i = 0; i < filmovi.length; i++) {
+        prikazFilmova.innerHTML += (`${filmovi[i].naziv}<br><br><img src="${filmovi[i].slika}" alt=""><br><br>${filmovi[i].godina}<br><br><br>`);
+
+    }
+}
+
+
+function sortirajAZ() {
+    filmovi.sort(function (a, b) {
+        var nameA = a.naziv.toUpperCase();
+        var nameB = b.naziv.toUpperCase();
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+
+
+        return 0;
+    });
+    prikazFilmova.innerHTML = "";
+    for (let i = 0; i < filmovi.length; i++) {
+        prikazFilmova.innerHTML += (`${filmovi[i].naziv}<br><br><img src="${filmovi[i].slika}" alt=""><br><br>${filmovi[i].godina}<br><br><br>`);
+
+    }
+}
+
+az = document.getElementById('iAZ');
+za = document.getElementById('iZA');
+az.addEventListener('click', sortirajAZ);
+za.addEventListener('click', sortirajZA);
+
+
+
+
+
+
+
+
